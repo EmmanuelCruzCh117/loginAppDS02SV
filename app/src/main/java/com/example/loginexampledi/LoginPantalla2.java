28b5e41ff3fbd60f32f3e2bac0bbea5aa85e620d
EmmanuelCruzCh117 <emmanuel.cruz18@hotmail.com>
2022-05-29 21:32:30 -0500

    Sign up done

--- app/src/main/java/com/example/loginexampledi/LoginPantalla2.java
+++ app/src/main/java/com/example/loginexampledi/LoginPantalla2.java
@@ -2,13 +2,26 @@ package com.example.loginexampledi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginPantalla2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        TextView textView;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_pantalla2);
+        textView = findViewById(R.id.textView);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginPantalla2.this, RegisterPantalla.class );
+                startActivity(intent);
+            }
+        });
     }
 }
